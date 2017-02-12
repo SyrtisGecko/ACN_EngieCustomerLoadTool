b58bc64f49d717a4f5b6c3580a881c67697937f2
SyrtisGecko <syrtismajor@gmail.com>
2017-02-12 20:58:37 +0100

    Createing new project

---
+++ src/MainGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Przemek on 2017-02-12.
+ */
+public class MainGUI {
+    private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
+    private JProgressBar progressBar3;
+    private JProgressBar progressBar4;
+    private JProgressBar progressBar5;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+}
