bb597fb4ce4ea692cf39e339b6ec3b7915999d3a
SyrtisGecko <syrtismajor@gmail.com>
2017-02-14 22:42:58 +0100

    Added selectBOInput_button ActionListener

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Przemek on 2017-02-12.
@@ -6,15 +8,16 @@ import javax.swing.*;
 public class MainGUI {
 
     JFrame frame;
+    int n;
 
     private JProgressBar BOprogressBar;
     private JProgressBar anagraficheProgressBar;
     private JProgressBar otherProgressBar;
     private JProgressBar savingProgressBar;
     private JProgressBar calculationProgressBar;
-    private JButton selectInput1_button;
-    private JButton selectInput2_button;
-    private JButton selectInput3_button;
+    private JButton selectBOInput_button;
+    private JButton selectAnagraficheInput_button;
+    private JButton selectOtherInput_button;
     private JButton selectOutput_button;
     private JButton calculate_button;
     private JPanel mainPanel;
@@ -27,10 +30,19 @@ public class MainGUI {
     private JLabel other_progress_label;
     private JLabel saving_progress_label;
     private JLabel calculation_progress_label;
-    private JTextPane processingLog;
+    private JTextArea processingLog;
 
     public MainGUI() {
         initGUI();
+
+        selectBOInput_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                n++;
+                System.out.println("Button was clicked");
+                processingLog.append("\nButton was clicked " +n+ " times.");
+            }
+        });
     }
 
     private void initGUI() {
@@ -42,6 +54,7 @@ public class MainGUI {
         frame.setResizable(false);
         frame.setVisible(true);
     }
+
 
 //    public static void main(String[] args) {
 //        JFrame frame = new JFrame("MainGUI");
