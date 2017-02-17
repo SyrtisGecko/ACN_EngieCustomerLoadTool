a7982de3f350e8444894d9ac4efaa0fba6011511
SyrtisGecko <syrtismajor@gmail.com>
2017-02-17 08:04:49 +0100

    Added new Record classes

---
+++ src/reportRecords/ReportGeneraleRecord.java
@@ -0,0 +1,12 @@
+package reportRecords;
+
+/**
+ * Created by Przemek on 2017-02-17.
+ */
+public class ReportGeneraleRecord {
+
+
+    public ReportGeneraleRecord(String record) {
+
+    }
+}
---
+++ src/reportRecords/ReportStatoClientiRecord.java
@@ -0,0 +1,11 @@
+package reportRecords;
+
+/**
+ * Created by Przemek on 2017-02-17.
+ */
+public class ReportStatoClientiRecord {
+
+    public ReportStatoClientiRecord(String record) {
+        
+    }
+}
